21c3a5e573ca9ba6f155898e301c5a1e6a8aff82
kevinren <330202207@qq.com>
2017-05-09 18:48:50 +0800

    edit

--- frontend/messages/zh-CN/common.php
+++ frontend/messages/zh-CN/common.php
@@ -7,8 +7,10 @@ return  [
     'Login' => '登录',
     'username' => '用户名',
     'password' => '密码',
+    'email' => '邮箱',
     'rememberMe' => '记住我',
     'Please fill out the following fields to login:' => '请填写以下字段进行登录：',
     'If you forgot your password you can' => '如果您忘记了密码，您可以',
     'reset it.' => '重新设置密码。',
+    'Please fill out the following fields to signup:' => '请填写以下字段注册：',
 ];
